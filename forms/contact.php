0427d7a8f29db1dc7a97ee6414c6127bccec87f8
Jomin Mathew <jominmathewmathew@gmail.com>
2023-05-26 10:09:15 -0700

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,38 +1,53 @@
 <?php
-  /**
+// Function to make API call to ChatGPT API
+function callChatGPTAPI($message) {
+    $api_key = 'YOUR_API_KEY'; // Replace with your actual API key
+    $endpoint = 'https://api.openai.com/v1/chat/completions';
 
-  */
+    $data = array(
+        'prompt' => $message,
+        'max_tokens' => 50,
+        'temperature' => 0.7,
+        'n' => 1
+    );
 
-  //  your real receiving email address
-  $receiving_email_address = 'jominmathew@lewisu.edu';
+    $headers = array(
+        'Content-Type: application/json',
+        'Authorization: Bearer ' . $api_key
+    );
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    $ch = curl_init($endpoint);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+    $response = curl_exec($ch);
+    curl_close($ch);
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+    return json_decode($response, true)['choices'][0]['text'];
+}
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+// Process the contact form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
-  echo $contact->send();
+    // Generate response using ChatGPT API
+    $response = callChatGPTAPI($message);
+
+    // Predefined details to include in the response
+    $predefinedDetails = "My Contact Details:\n";
+    $predefinedDetails .= "Name: Your Name\n";
+    $predefinedDetails .= "Email: yourname@example.com\n";
+    $predefinedDetails .= "Phone: +1-123-456-7890\n";
+
+    // Combine the response and predefined details
+    $combinedResponse = $response . "\n\n" . $predefinedDetails;
+
+    // Send the response to the user (e.g., via email or displaying on the webpage)
+    echo $combinedResponse;
+}
 ?>
